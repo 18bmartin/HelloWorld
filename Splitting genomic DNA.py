a90081be0caeffdc77658113d5e6214e0d5a7da4
18bmartin <18bmartin@mlmc.vic.edu.au>
2017-11-23 12:36:15 +1100

    Completing the exercise 2-1 code

---
+++ Splitting genomic DNA.py
@@ -0,0 +1,16 @@
+#finding and opening the intial file
+DNAfile = open('/users/18bmartin/downloads/genomic_dna.txt', 'r')
+DNAsequence = DNAfile.read()
+
+#splitting the file data into exons and an intron
+exon1 = DNAsequence[0:63]
+intron = DNAsequence[63:90]
+exon2 = DNAsequence[90:]
+
+#creating a file for the exons
+exonFile = open('exonFile.txt', 'w')
+exonFile.write(exon1 + exon2)
+
+#creating a file for the intron
+intronFile = open('intronFile.txt', 'w')
+intronFile.write(intron)
