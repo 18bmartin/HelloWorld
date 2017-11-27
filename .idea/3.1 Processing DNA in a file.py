1678831afd15619361c28d20b388e03caa3388ce
18bmartin <18bmartin@mlmc.vic.edu.au>
2017-11-28 08:10:14 +1100

    Completing the exercise 3-1 code

--- .idea/3.1 Processing DNA in a file.py
+++ .idea/3.1 Processing DNA in a file.py
@@ -1,9 +1,14 @@
+#open the files
 inFile = open('/users/18bmartin/downloads/ex3-1_input.txt', 'r')
 outFile = open('trim.txt.', 'w')
 
+#trim, add the new line to the file, print the line and the length of the line
 for line in inFile:
     line = line[14:]
     outfile = outFile.write(line)
+    print("line is", len(line), "chratcers long.")
+    print(line)
 
+#close the files
 outFile.close()
 inFile.close()
